b5a3720ba88d4b3d7a8a7c37c5c4a86fd12c84c9
yeonji0 <kyj0279@gmail.com>
2022-07-06 18:16:52 +0900

    d

---
+++ tour/include/footer.php
@@ -0,0 +1,21 @@
+<footer>
+        <div class="footerWrap">
+            <div class="ftTop">
+                <a href="#"><img src="images/logo.png" alt=""></a>
+                    <p>
+                        <a href="#">이용약관<span></span></a>
+                        <a href="#">개인정보취급방침<span></span></a>
+                        <a href="#">사이트맵<span></span></a>
+                        <a href="#">고객센터<span></span></a>
+                        <a href="#">광고 및 제휴 문의</a>
+                    </p>
+            </div>
+    
+            <div class="ftadd"> 
+                    <p>123-456 서울특별시 구로구 구로동 (주) 하이미디어 사업지등록번호 : 123-4558-56596 대표이사 : 홍길동 대표번호 : 02-4855-05965</p>
+                    <p>Copyrightⓒ NHN Doctortour Corp. All rights reserved.</p>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
